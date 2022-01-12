2ef98ee4697907419651424dd5e86bb6fe21006f
sharan karthikeyan <karthikeyansharan00@gmail.com>
2022-01-12 20:53:08 +0530

    pointers

---
+++ 5. pointers/pointers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int *a;
+    char *b;
+    float *c;
+    double *d;
+
+    long double val = 23123;
+    long double *e = &val;
+    cout<<sizeof(a)<<" "<<sizeof(b)<<" "<<sizeof(c)<<" "<<sizeof(d)<<" "<<sizeof(e)<<endl; // sizeof address present inside the pointer.
+    cout<<sizeof(&a)<<" "<<sizeof(&b)<<" "<<sizeof(&c)<<" "<<sizeof(&d)<<" "<<sizeof(&e)<<endl; // sizeof the address of the pointer
+    cout<<sizeof(*a)<<" "<<sizeof(*b)<<" "<<sizeof(*c)<<" "<<sizeof(*d)<<" "<<sizeof(*e)<<endl; // sizeof the data present inside that pointer holds address.
+    return 0;
+}
