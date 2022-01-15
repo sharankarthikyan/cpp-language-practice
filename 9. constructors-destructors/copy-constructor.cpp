20f2c4c8a454140c33142ff02a85b15b60066926
sharan karthikeyan <karthikeyansharan00@gmail.com>
2022-01-15 14:07:09 +0530

    shallow copy

---
+++ 9. constructors-destructors/copy-constructor.cpp
@@ -0,0 +1,71 @@
+// CPP also us to delegate the constructors for initialization list.
+
+#include <iostream>
+using namespace std;
+
+class Player {
+private:
+  string name;
+  int health;
+  int xp;
+
+public:
+  std::string getName() { return name; }
+  int getHealth() { return health; }
+  int getXp() { return xp; }
+
+  // delegated constructor with default values <- very very efficient
+  Player(std::string n = "None", int health_val = 0,
+         int xp_val = 0); // This constructor will be called, when we creating
+                          // objects with 0, 1, 2, 3 args.
+
+  // COPY CONSTRUCTOR
+  Player(const Player &source);
+
+  // destructor
+  ~Player() { cout << "Destructor called for " << name << endl; }
+};
+
+// delegated constructor with default values <- very very efficient
+Player::Player(std::string n, int health_val, int xp_val)
+    : name{n}, health{health_val}, xp{xp_val} {
+  cout << "Three args constructor" << endl;
+}
+
+// Player::Player(const Player &source)
+//     : name{source.name}, health{source.health}, xp{source.xp} {
+//   cout << "Copy - Constructor called and made a copy of" << source.name <<
+//   endl;
+// }
+
+// We can also simply write a above copy constructor like below
+Player::Player(const Player &source)
+    : Player{source.name, source.health, source.xp} {
+  cout << "Copy - Constructor called and made a copy of" << source.name << endl;
+}
+
+void displayPlayer(
+    Player p) { // Player p = new Player(sharan) OR Player p{sharan}
+                // HERE: sharan is arg while calling
+                // You can see, we have creating an local object(args) for
+                // Player. But, we need to make a copy. Reason is, if we change
+                // anything without making a copy, it will affects the real
+                // object which is calling. Finally, In this line we are calling
+                // copy constructor and Got a copied object. This object gets
+                // deleted when this local scope ends.
+  cout << "Name: " << p.getName() << endl;
+  cout << "Health: " << p.getHealth() << endl;
+  cout << "XP: " << p.getXp() << endl;
+}
+
+int main() {
+  Player sharan;
+  displayPlayer(sharan);
+
+  Player sharan_copy{sharan}; // copy constructor called
+  displayPlayer(sharan_copy);
+
+  Player dhoni{"Dhoni"};
+  Player raina{"Raina", 100, 100};
+  return 0;
+}
---
+++ 9. constructors-destructors/shallow-copy.cpp
@@ -0,0 +1,48 @@
+// Shallow copy - only the pointer is copied - not what is pointing to!
+// Problem: source and newly created object BOTH point to the SAME data area.
+
+#include <iostream>
+using namespace std;
+
+class Shallow {
+private:
+  int *data;
+
+public:
+  void setDataValue(int d) { *data = d; }
+  int getDataValue() { return *data; }
+
+  Shallow(int d);
+
+  // COPY CONSTRUCTOR
+  Shallow(const Shallow &source);
+
+  // destructor
+  ~Shallow();
+};
+
+Shallow::Shallow(int d) {
+  data = new int;
+  *data = d;
+}
+
+Shallow::Shallow(const Shallow &source) : data{source.data} {
+  cout << "Copy Constructor - Shallow copy" << endl;
+}
+
+Shallow::~Shallow() {
+  delete data;
+  cout << "Destructor freeing data" << endl;
+}
+
+void displayShallow(Shallow s) { cout << s.getDataValue() << endl; }
+
+int main() { // This shallow copy will cause an error.
+  Shallow obj1{100};
+  displayShallow(obj1);
+
+  Shallow obj2{obj1};
+  obj2.setDataValue(1000);
+
+  return 0;
+}
